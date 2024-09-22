8ce7dfdc79e4583802dfddbbcaa7239c4946ccd9
Saurabh <saurabhrjadhav26@gmail.com>
2024-09-22 11:51:51 +0530

    Update updateSongsList.py

--- modules/updateSongsList.py
+++ modules/updateSongsList.py
@@ -4,13 +4,13 @@ def updateSongsList(osuDir):
   d = []
   string = ''
 
-  for (dirpath, dirnames, filenames) in walk(osuDir + '\Songs'):
+  for (dirpath, dirnames, filenames) in walk(osuDir + '/Songs'):
       d.extend(dirnames)
       break
 
   for i in d:
     string += f'{i}\n'
 
-  dumpFile = open('./data/songsList.txt', 'w')
+  dumpFile = open('././data/songsList.txt', 'w')
 
   dumpFile.write(string)
