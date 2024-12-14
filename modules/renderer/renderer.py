8102b6ccda2117fa31f5bbecf8b7597bcd7a692f
Saurabh <saurabhrjadhav26@gmail.com>
2024-12-14 17:41:25 +0530

    Update renderer.py

--- modules/renderer/renderer.py
+++ modules/renderer/renderer.py
@@ -20,28 +20,29 @@ class Section:
       self.absolutePositioning = True
 
     if absolutePositioning:
-      self.rect.x = self.x = xRatio
-      self.rect.y = self.y = yRatio
-      self.rect.width = self.width = widthRatio
-      self.rect.height = self.height = heightRatio
+      self.x, self.y = xRatio, yRatio
+      self.width, self.height = widthRatio, heightRatio
+      self.rect.update(self.x, self.y, self.width, self.height)
     elif container:
-      self.rect.x = self.x = container.x + container.width * xRatio
-      self.rect.y = self.y = container.y + container.height * yRatio
-      self.rect.width = self.width = container.width * widthRatio
-      self.rect.height = self.height = container.height * heightRatio
+      self.x = container.x + container.width * xRatio
+      self.y = container.y + container.height * yRatio
+      self.width = container.width * widthRatio
+      self.height = container.height * heightRatio
       self.container = container
       self.xRatio = xRatio
       self.yRatio = yRatio
       self.widthRatio = widthRatio
       self.heightRatio = heightRatio
+      self.rect.update(self.x, self.y, self.width, self.height)
 
   def updateDim(self):
     if not self.absolutePositioning:
-      self.rect.x = self.x = self.container.x + self.container.width * self.xRatio
-      self.rect.y = self.y = self.container.y + self.container.height * self.yRatio
-      self.rect.width = self.width = self.container.width * self.widthRatio
-      self.rect.height = self.height = self.container.height * self.heightRatio
-  
+      self.x = self.container.x + self.container.width * self.xRatio
+      self.y = self.container.y + self.container.height * self.yRatio
+      self.width = self.container.width * self.widthRatio
+      self.height = self.container.height * self.heightRatio
+      self.rect.update(self.x, self.y, self.width, self.height)
+
   def draw(self, surface: pg.Surface):
     if isinstance(self.background, pg.Surface):
       surface.blit(self.background)
@@ -49,8 +50,38 @@ class Section:
       pg.draw.rect(surface, self.background, self.rect)
 
 class Button:
-  def __init__(self, section: Section, text: str, onClick: Optional[Callable] = None, border: int = 0):
+  def __init__(self, section: Section, text: str, pressedColor: pg.Color, borderColor: pg.Color, borderColorPressed: pg.Color, onClick: Optional[Callable] = None, border: int = 0):
     self.section = section
     self.text = text
     self.onClick = onClick
     self.border = border
+    self.pressed = False
+    self.defaultBackground = section.background
+    self.pressedBackground = pressedColor
+    self.borderColor = borderColor
+    self.borderColorPressed = borderColorPressed
+    self.borderRect = pg.Rect(section.x - border, section.y - border, section.width + (border * 2), section.height + (border * 2))
+
+  def checkEvent(self, event: pg.event.Event):
+    if event.type == pg.MOUSEBUTTONDOWN and event.button == 1 and self.section.rect.collidepoint(event.pos):
+      self.pressed = True
+      self.section.background = self.pressedBackground
+      if self.onClick:
+        self.onClick()
+    elif event.type == pg.MOUSEBUTTONUP and self.pressed:
+      self.pressed = False
+      self.section.background = self.defaultBackground
+
+  def updateDim(self):
+    self.section.updateDim()
+    newX, newY = self.section.x - self.border, self.section.y - self.border
+    newWidth, newHeight = self.section.width + (self.border * 2), self.section.height + (self.border * 2)
+    self.borderRect.update(newX, newY, newWidth, newHeight)
+
+  def draw(self, surface):
+    if self.border > 0:
+      if self.pressed:
+        pg.draw.rect(surface, self.borderColorPressed, self.borderRect)
+      else:
+        pg.draw.rect(surface, self.borderColor, self.borderRect)
+    self.section.draw(surface)
