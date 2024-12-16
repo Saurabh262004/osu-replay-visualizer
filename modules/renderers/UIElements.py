2832aaf6762f7fe590f4d0e9bd891e6150a4cd58
Saurabh <saurabhrjadhav26@gmail.com>
2024-12-16 19:18:09 +0530

    rea-named renderer.py to UIElements.py
    + added text rendering for Button and new Toggle element

--- modules/renderers/UIElements.py
+++ modules/renderers/UIElements.py
@@ -6,10 +6,6 @@ numType = Union[int, float]
 containerType = Union['Section', pg.Rect]
 backgroundType = Union[pg.Color, pg.Surface]
 
-def draw_text(screen, text, font, textCol, x, y):
-  img = font.render(text, True, textCol)
-  screen.blit(img, (x, y))
-
 class Section:
   def __init__(self, xRatio: numType, yRatio: numType, widthRatio: numType, heightRatio: numType, container: Optional[containerType], background: backgroundType, absolutePositioning: Optional[bool] = True):
     self.absolutePositioning = absolutePositioning
@@ -50,9 +46,8 @@ class Section:
       pg.draw.rect(surface, self.background, self.rect)
 
 class Button:
-  def __init__(self, section: Section, text: str, pressedColor: pg.Color, borderColor: pg.Color, borderColorPressed: pg.Color, onClick: Optional[Callable] = None, border: int = 0):
+  def __init__(self, section: Section, text: str, fontPath: str, textColor: pg.Color, pressedColor: pg.Color, borderColor: pg.Color, borderColorPressed: pg.Color, onClick: Optional[Callable] = None, border: int = 0):
     self.section = section
-    self.text = text
     self.onClick = onClick
     self.border = border
     self.pressed = False
@@ -61,6 +56,12 @@ class Button:
     self.borderColor = borderColor
     self.borderColorPressed = borderColorPressed
     self.borderRect = pg.Rect(section.x - border, section.y - border, section.width + (border * 2), section.height + (border * 2))
+
+    self.text = text
+    self.textColor = textColor
+    self.fontPath = fontPath
+
+    self.updateDim()
 
   def checkEvent(self, event: pg.event.Event):
     if event.type == pg.MOUSEBUTTONDOWN and event.button == 1 and self.section.rect.collidepoint(event.pos):
@@ -74,9 +75,17 @@ class Button:
 
   def updateDim(self):
     self.section.updateDim()
+
     newX, newY = self.section.x - self.border, self.section.y - self.border
     newWidth, newHeight = self.section.width + (self.border * 2), self.section.height + (self.border * 2)
+
     self.borderRect.update(newX, newY, newWidth, newHeight)
+
+    self.fontSize = max(10, int(self.section.height * .6))
+    self.font = pg.font.SysFont(self.fontPath, self.fontSize)
+
+    self.textSurface = self.font.render(self.text, True, self.textColor)
+    self.textRect = self.textSurface.get_rect(center = self.section.rect.center)
 
   def draw(self, surface):
     if self.border > 0:
@@ -84,4 +93,49 @@ class Button:
         pg.draw.rect(surface, self.borderColorPressed, self.borderRect)
       else:
         pg.draw.rect(surface, self.borderColor, self.borderRect)
+
     self.section.draw(surface)
+
+    surface.blit(self.textSurface, self.textRect)
+
+class Toggle:
+  def __init__(self, section: Section, toggledColor: pg.Color, borderColor: pg.Color, borderColorToggled: pg.Color, onClick: Optional[Callable] = None, border: int = 0):
+    self.section = section
+    self.onClick = onClick
+    self.border = border
+    self.toggled = False
+    self.defaultBackground = section.background
+    self.toggledBackground = toggledColor
+    self.borderColor = borderColor
+    self.borderColorToggled = borderColorToggled
+    self.borderRect = pg.Rect(section.x - border, section.y - border, section.width + (border * 2), section.height + (border * 2))
+
+  def checkEvent(self, event: pg.event.Event):
+    if event.type == pg.MOUSEBUTTONDOWN and event.button == 1 and self.section.rect.collidepoint(event.pos):
+      self.toggled = not self.toggled
+
+      if self.toggled:
+        self.section.background = self.toggledBackground
+      else:
+        self.section.background = self.defaultBackground
+
+      if self.onClick:
+        self.onClick(self.toggled)
+
+  def updateDim(self):
+    self.section.updateDim()
+
+    newX, newY = self.section.x - self.border, self.section.y - self.border
+    newWidth, newHeight = self.section.width + (self.border * 2), self.section.height + (self.border * 2)
+
+    self.borderRect.update(newX, newY, newWidth, newHeight)
+
+  def draw(self, surface):
+    if self.border > 0:
+      if self.toggled:
+        pg.draw.rect(surface, self.borderColorToggled, self.borderRect)
+      else:
+        pg.draw.rect(surface, self.borderColor, self.borderRect)
+
+    self.section.draw(surface)
+
