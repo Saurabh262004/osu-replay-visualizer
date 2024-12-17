a35a301d2ce7aa4dda76ed33cb0bfb1fd1753fb5
Saurabh <saurabhrjadhav26@gmail.com>
2024-12-17 12:57:40 +0530

    Dynamic dimensions
    The dimensions for the Section can now be individually relative to any other dimension of the container or be absolute.

--- modules/renderers/UIElements.py
+++ modules/renderers/UIElements.py
@@ -1,49 +1,102 @@
 # a module that helps render stuff on screen with pygame
 import pygame as pg
-from typing import Union, Optional, Callable
+from typing import Union, Optional, Callable, Dict
 
 numType = Union[int, float]
 containerType = Union['Section', pg.Rect]
 backgroundType = Union[pg.Color, pg.Surface]
 
+DIMENSIONS_ALLOWED_KEYVALS = (
+  ('x', 'y', 'w', 'h'),
+  ('type', 'value'),
+  ('a', 'r')
+)
+
 class Section:
-  def __init__(self, xRatio: numType, yRatio: numType, widthRatio: numType, heightRatio: numType, container: Optional[containerType], background: backgroundType, absolutePositioning: Optional[bool] = True):
-    self.absolutePositioning = absolutePositioning
+  def __init__(self, dimensions: Dict[str, Dict[str, Union[str, int, float]]], container: Optional[containerType], background: backgroundType):
+    self.dimensions = dimensions
+    self.container = container
     self.background = background
     self.rect = pg.Rect(0, 0, 0, 0)
 
-    if not container:
-      self.absolutePositioning = True
+    if not self.__validDims():
+      print('invalid dimension object')
+      return None
 
-    if absolutePositioning:
-      self.x, self.y = xRatio, yRatio
-      self.width, self.height = widthRatio, heightRatio
-      self.rect.update(self.x, self.y, self.width, self.height)
-    elif container:
-      self.x = container.x + container.width * xRatio
-      self.y = container.y + container.height * yRatio
-      self.width = container.width * widthRatio
-      self.height = container.height * heightRatio
-      self.container = container
-      self.xRatio = xRatio
-      self.yRatio = yRatio
-      self.widthRatio = widthRatio
-      self.heightRatio = heightRatio
-      self.rect.update(self.x, self.y, self.width, self.height)
+    if not container:
+      dim = self.dimensions
+      for d in dim:
+        if not dim[d]['type'] == 'a':
+          dim[d]['type'] = 'a'
+
+    self.updateDim()
 
   def updateDim(self):
-    if not self.absolutePositioning:
-      self.x = self.container.x + self.container.width * self.xRatio
-      self.y = self.container.y + self.container.height * self.yRatio
-      self.width = self.container.width * self.widthRatio
-      self.height = self.container.height * self.heightRatio
-      self.rect.update(self.x, self.y, self.width, self.height)
+    dim = self.dimensions
+    for d in dim:
+      typ = dim[d]['type']
+      if typ == 'a':
+        dim[d]['calcVal'] = dim[d]['value']
+      elif typ[0] == 'r':
+        rel = self.__getRel(typ[1])
+        val = dim[d]['value']
+        padding = self.__getPadding(d)
+
+        dim[d]['calcVal'] = padding + (rel * val)
+
+    self.x = self.dimensions['x']['calcVal']
+    self.y = self.dimensions['y']['calcVal']
+    self.width = self.dimensions['w']['calcVal']
+    self.height = self.dimensions['h']['calcVal']
+
+    self.rect.update(self.x, self.y, self.width, self.height)
 
   def draw(self, surface: pg.Surface):
     if isinstance(self.background, pg.Surface):
       surface.blit(self.background)
     elif isinstance(self.background, pg.Color):
       pg.draw.rect(surface, self.background, self.rect)
+
+  def __getRel(self, typ):
+    if typ == 'x':
+      return self.container.x
+    if typ == 'y':
+      return self.container.y
+    if typ == 'w':
+      return self.container.width
+    if typ == 'h':
+      return self.container.height
+
+  def __getPadding(self, key):
+    if key == 'x':
+      return self.container.x
+    if key == 'y':
+      return self.container.y
+    return 0
+
+  def __validDims(self):
+    dim = self.dimensions
+    if (set(self.dimensions.keys()) == set(DIMENSIONS_ALLOWED_KEYVALS[0])):
+      for d in dim:
+        if (set(dim[d].keys()) == set(DIMENSIONS_ALLOWED_KEYVALS[1])):
+          if dim[d]['type'][0] in DIMENSIONS_ALLOWED_KEYVALS[2]:
+            if dim[d]['type'][0] == 'r':
+              if (len(dim[d]['type']) == 2) and dim[d]['type'][1] in DIMENSIONS_ALLOWED_KEYVALS[0]:
+                return True
+              else: break
+            return True
+          else: break
+        else: break
+    return False
+
+  @staticmethod
+  def createDimObject(arr):
+    return {
+      'x': {'type': arr[0], 'value': arr[1]},
+      'y': {'type': arr[2], 'value': arr[3]},
+      'w': {'type': arr[4], 'value': arr[5]},
+      'h': {'type': arr[6], 'value': arr[7]}
+    }
 
 class Button:
   def __init__(self, section: Section, text: str, fontPath: str, textColor: pg.Color, pressedColor: pg.Color, borderColor: pg.Color, borderColorPressed: pg.Color, onClick: Optional[Callable] = None, border: int = 0):
