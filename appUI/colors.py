cb7ea2ed1867448bc3002d3c394894069f6234c2
Saurabh <saurabhrjadhav26@gmail.com>
2025-01-10 23:04:16 +0530

    small fix

--- appUI/colors.py
+++ appUI/colors.py
@@ -9,9 +9,9 @@ class AppColors:
 
     self.background1 = C(31, 31, 31)
 
-    self.listElement1 = C(50, 50, 50),
-    self.listElement1Heighlight1 = C(55, 55, 55),
-    self.listElement1Heighlight2 = C(60, 60, 60),
+    self.listElement1 = C(50, 50, 50)
+    self.listElement1Heighlight1 = C(55, 55, 55)
+    self.listElement1Heighlight2 = C(60, 60, 60)
 
     self.primary1 = C(50, 150, 200)
 
